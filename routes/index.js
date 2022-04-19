f8148ac9886ebf90ca407d66c3cdedaf7519bc84
ji <jiyongprk@gmail.com>
2022-04-19 17:18:57 -0400

    upload files

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req,res,next){
+  res.render('new', )
+})
+
+module.exports = router;
