b890887c79dd9d48d17f5958beba54369fdcfa33
ji <jiyongprk@gmail.com>
2022-04-19 19:27:45 -0400

    posting working

--- routes/index.js
+++ routes/index.js
@@ -5,22 +5,34 @@ const messages = [
   {
     text: "Hi there!",
     user: "Amando",
-    added: new Date()
+    added: new Date().getDate()
   },
   {
     text: "Hello World!",
     user: "Charles",
-    added: new Date()
+    added: new Date().getDate()
   }
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
 
-router.get('/new', (req,res,next){
+router.get('/new', (req, res)=>{
   res.render('form')
+});
+
+router.post('/new',(req,res)=>{
+  let newMessage = req.body.text
+  let user = req.body.user
+
+  messages.push({
+    text: newMessage,
+    user: user,
+    added: new Date().getDate()
+  })
+  res.redirect('/')
 })
 
 module.exports = router;
